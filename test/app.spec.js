7eb21ddb7982f736e689fb8599245f9956c6f061
BlancaCarrasco <blankcarr@gmail.com>
2019-02-07 16:36:59 -0300

    prueba de test

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,2 +1,22 @@
+describe('registrar', () => {
 
+  it('debería ser una función', () => {
+    assert.equal(typeof registrar, 'function');
+  });
+
+  it('debería poder registrar con el correo electronico "blankcarr@gmail.com" con su clave 123456', () => {
+    assert.equal(registrar.registrado(" blankcarr@gmail.com", 123456), "usuario registrado");
+  });
+});
+
+describe('login', () => {
+
+  it('debería ser una función', () => {
+    assert.equal(typeof login, 'function');
+  });
+
+  it('debería poder entrar con el correo electronico "blankcarr@gmail.com" con su clave 123456', () => {
+    assert.equal(entrar.login(" blankcarr@gmail.com", 123456), "sesion iniciada");
+  });
+});
 
